f1df7c6c079b41c5c0ad85ed4a3dec747570014f
cristi <tataru.cristi5@gmail.com>
2021-03-11 20:57:31 +0200

    initial check-in of the FeaturesApp code

---
+++ featureApp/src/main/java/com/tataru/FeatureAppApplication.java
@@ -0,0 +1,13 @@
+package com.tataru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeatureAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeatureAppApplication.class, args);
+	}
+
+}
---
+++ featureApp/src/test/java/com/tataru/FeatureAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tataru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeatureAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
